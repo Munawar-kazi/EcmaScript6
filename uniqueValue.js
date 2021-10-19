dde68e12a71e0b6dc16b4643c065a91e7af06862
Munawar-kazi <kazimnwr@gmail.com>
2021-10-19 19:11:07 -0400

    munawar kzi chnges

---
+++ uniqueValue.js
@@ -0,0 +1,32 @@
+let Array=["a","b","c","d","e","a","c","a","f"];
+// console.log(Array);
+
+let Array1=["g","h","i","j","k","g","h","i","l","m"];
+// console.log(Array1);
+
+let spread=[...Array,...Array1];  //using spread operator for concatonation two or more arrays
+console.log(spread);
+let unique=[];
+// unique.push("ayaan","kazi","ayaan");
+// console.log(unique)
+
+// Array.forEach(element => {
+//     if(!unique.includes(element)){
+//         unique.push(element)
+//     };
+    
+// });
+// console.log(unique);
+
+spread.forEach(element=>{               //separated unique value inludes
+    if(!unique.includes(element)){
+        unique.push(element)
+    }
+});
+
+console.log(unique);
+
+for(key in unique){
+    console.log(unique[key]);
+}
+///munawar kazi////
